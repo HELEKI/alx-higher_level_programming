8aaef7bdcb1f6fe0249098833a42ed0f3fc48c9d
HELEKI <helekiobura@gmail.com>
2022-06-27 20:42:58 -0700

    My header file

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -8,6 +8,7 @@
  * @n: integer
  * @next: points to the next node
  * Description: singly linked list node structure
+ * for Holberton project
  */
 
 typedef struct listint_s
@@ -21,7 +22,6 @@ typedef struct listint_s
 } listint_t;
 
 
-
 size_t print_listint(const listint_t *h);
 
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -32,4 +32,4 @@ int check_cycle(listint_t *list);
 
 
 
-#endif /*LISTS_H*/
+#endif /* LISTS_H */
