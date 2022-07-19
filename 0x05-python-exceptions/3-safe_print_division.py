088cd487648c30081fcc393e0597a8990b769ce6
HELEKI <helekiobura@gmail.com>
2022-07-19 12:07:46 -0700

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+global div
+
+def safe_print_division(a, b):
+    global div
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return 
